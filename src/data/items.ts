9614e93de16ecdd3b7bafd22f5d08ed845b96fd3
Rodrigo Soares <meuemail.rodrigodev@gmail.com>
2022-09-27 22:46:00 -0300

    deploy

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+    {
+        date: new Date(2022, 9, 24),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12
+    },
+    {
+        date: new Date(2022, 9, 24),
+        category: 'food',
+        title: 'Burger King',
+        value: 28
+    },
+    {
+        date: new Date(2022, 9, 24),
+        category: 'rent',
+        title: 'Aluguel apt',
+        value: 1000
+    },
+    {
+        date: new Date(2022, 9, 24),
+        category: 'salary',
+        title: 'Salario',
+        value: 4500
+    },
+];
